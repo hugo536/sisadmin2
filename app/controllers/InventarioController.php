23f04a4e612d5b9105959eccaaa630e4d1194bbe
hugo536 <snwalter55@gmail.com>
2026-02-12 09:09:34 -0500

    Eliminar tarjetas de resumen del módulo inventario

--- app/controllers/InventarioController.php
+++ app/controllers/InventarioController.php
@@ -31,7 +31,6 @@ class InventarioController extends Controlador
             'stockActual' => $stockActual,
             'almacenes' => $this->almacenModel->listarActivos(),
             'items' => $this->inventarioModel->listarItems(),
-            'kpis' => $this->calcularKpis($stockActual),
             'flash' => ['tipo' => '', 'texto' => ''],
         ];
 
@@ -203,49 +202,6 @@ class InventarioController extends Controlador
         }
 
         fclose($out);
-    }
-
-    private function calcularKpis(array $stockActual): array
-    {
-        $mapaItems = [];
-        $sinStock = 0;
-        $critico = 0;
-        $porVencer = 0;
-        $hoy = new DateTimeImmutable('today');
-
-        foreach ($stockActual as $stock) {
-            $idItem = (int) ($stock['id_item'] ?? 0);
-            $stockItem = (float) ($stock['stock_actual'] ?? 0);
-            $stockMin = (float) ($stock['stock_minimo'] ?? 0);
-            if (!isset($mapaItems[$idItem])) {
-                $mapaItems[$idItem] = 0.0;
-            }
-            $mapaItems[$idItem] += $stockItem;
-
-            if ($stockItem <= 0) {
-                $sinStock++;
-            }
-
-            if ($stockItem > 0 && $stockItem <= $stockMin) {
-                $critico++;
-            }
-
-            $venc = (string) ($stock['proximo_vencimiento'] ?? '');
-            $diasAlerta = (int) ($stock['dias_alerta_vencimiento'] ?? 0);
-            if ($venc !== '') {
-                $fechaV = DateTimeImmutable::createFromFormat('Y-m-d', $venc);
-                if ($fechaV instanceof DateTimeImmutable && $fechaV <= $hoy->modify('+' . max(1, $diasAlerta) . ' days')) {
-                    $porVencer++;
-                }
-            }
-        }
-
-        return [
-            'total_items' => count($mapaItems),
-            'sin_stock' => $sinStock,
-            'critico' => $critico,
-            'por_vencer' => $porVencer,
-        ];
     }
 
     private function vista(string $rutaVista, array $datos = []): void
