e26c4299b959d094bb94406448ac42f5e0782a04
hugo536 <snwalter55@gmail.com>
2026-02-24 09:13:41 -0500

    Fix parse error in ComprasOrdenModel

--- app/models/ComprasOrdenModel.php
+++ app/models/ComprasOrdenModel.php
@@ -386,11 +386,6 @@ class ComprasOrdenModel extends Modelo
         }
 
         if (!$this->tablaTieneColumna('items_unidades', 'id') || !$this->tablaTieneColumna('items_unidades', 'id_item')) {
-        if (!$this->tablaExiste('items_unidades')) {
-            return [];
-        }
-
-        if (!$this->tablaTieneColumna('items_unidades', 'id_item')) {
             return [];
         }
 
